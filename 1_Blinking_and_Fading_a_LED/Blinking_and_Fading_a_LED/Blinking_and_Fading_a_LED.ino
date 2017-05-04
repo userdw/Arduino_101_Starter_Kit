77028e74628fb0d9c14ed0a755f0a8f3cd3d7bed
IETrainingDiv <insidetrainingie@gmail.com>
2017-05-04 09:05:21 +0700

    Add files via upload

---
+++ 1_Blinking_and_Fading_a_LED/Blinking_and_Fading_a_LED/Blinking_and_Fading_a_LED.ino
@@ -0,0 +1,32 @@
+#define LED_MODULE 9
+
+void toggleLED(int times) {
+  for (int i = 0; i < times; i++) {
+    digitalWrite(LED_MODULE, !digitalRead(LED_MODULE));
+    delay(250);
+    digitalWrite(LED_MODULE, !digitalRead(LED_MODULE));
+    delay(250);
+  }
+}
+
+void fadeLED(int times) {
+  for (int i = 0; i < times; i++) {
+    for (int j = 0; j < 256; j++) {
+      analogWrite(LED_MODULE, j);
+      delay(1);
+    }
+    for (int j = 255; j > 0; j--) {
+      analogWrite(LED_MODULE, j);
+      delay(1);
+    }
+  }
+}
+
+void setup() {
+  pinMode(LED_MODULE, OUTPUT);
+}
+
+void loop() {
+  toggleLED(4);
+  fadeLED(4);
+}
